fa01aecec37d817b6372cf0c856fd3b17867dfad
CharlesGonzaga0 <113580578+CharlesGonzaga0@users.noreply.github.com>
2022-12-20 15:19:33 +0800

    Add files via upload
    Activity 3

---
+++ Activity.java
@@ -0,0 +1,73 @@
+package Activity;
+import java.util.Scanner;
+public class Activity {
+
+
+
+	
+
+	    public static void main(String[] args) {
+
+	        //Charles Erwin G. Gonzaga
+
+	        method();
+
+	    }
+
+	    public static void printer(String p) {
+	        System.out.print(p);
+	    }
+
+	    public static void method() {
+	        Scanner sc = new Scanner(System.in);
+	        printer("Enter Source Language: ");
+	        String i = sc.nextLine();
+	        printer("Output is: ");
+	        String input[] = i.split(" ");
+	        String choices[] = { "String", "int", "char", "double" };
+	        int val = 0;
+	        String array[] = new String[5];
+	        int x = 0;
+	        String CONSTANT = "<data_type>";
+	        for (String string : input) {
+
+	            if (string.contains(choices[0])) {
+	                array[x] = CONSTANT;
+
+	            } else if (string.contains(choices[1])) {
+	                array[x] = CONSTANT;
+
+	            } else if (string.contains(choices[2])) {
+	                array[x] = CONSTANT;
+
+	            } else if (string.contains(choices[3])) {
+	                array[x] = CONSTANT;
+
+	            } else if (string.contains("=")) {
+	                array[x + 2] = "<assignment_operator>";
+
+	            } else if (string.contains(";")) {
+	                if (val == 0 && string.contains("\"") || string.contains(";")
+	                        || string.contains("'")) {
+	                    array[x + 3] = "<value>";
+	                    array[x + 4] = "<delimiter>";
+
+	                } else if (string.contains(";")) {
+	                    array[x + 4] = "<delimiter>";
+
+	                }
+	            } else if (val == 0 && string.contains("\"") || string.contains("'")) {
+	                val++;
+	                array[x + 3] = "<value>";
+
+	            } else {
+	                array[x + 1] = "<identifier>";
+	            }
+
+	        }
+	        sc.close();
+	        for (String string : array) {
+	            printer(string);
+	        }
+	    }
+	}
